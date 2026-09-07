9f7b9ffbcbd810ef2d6b658b7911af2932bc7b7f
tgrab1 <grabowst@purdue.edu>
2026-09-07 15:08:22 -0400

    CSP-10 Experience Functions added.

--- exp.py
+++ exp.py
@@ -1,1 +1,34 @@
-print("This is about the exp.py file. It does not contain any functionality yet.")
+experiences = []
+
+
+def add_experience(company, role, years):
+    experience = {
+        "company": company,
+        "role": role,
+        "years": years
+    }
+
+    experiences.append(experience)
+    print("Experience added successfully.")
+
+
+def display_experiences():
+    print("User Experience:")
+
+    for experience in experiences:
+        print(
+            f"{experience['role']} at "
+            f"{experience['company']} - "
+            f"{experience['years']} years"
+        )
+
+
+def main():
+    add_experience("Tech Company", "Software Intern", 1)
+    add_experience("Startup Inc.", "Developer", 2)
+
+    display_experiences()
+
+
+if __name__ == "__main__":
+    main()
